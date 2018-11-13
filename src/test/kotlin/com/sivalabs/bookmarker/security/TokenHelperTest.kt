5c6f1a4a6566acdbfc956901c3e292b10ef0026a
Siva Katamreddy <siva.katamreddy@n26.com>
2018-11-13 15:27:43 +0100

    Add tokenHelper unit test

---
+++ src/test/kotlin/com/sivalabs/bookmarker/security/TokenHelperTest.kt
@@ -0,0 +1,29 @@
+package com.sivalabs.bookmarker.security
+
+import com.sivalabs.bookmarker.config.TimeProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.springframework.test.util.ReflectionTestUtils
+
+class TokenHelperTest {
+    lateinit var tokenHelper: TokenHelper
+
+    @Before
+    fun setUp() {
+        tokenHelper = TokenHelper()
+        ReflectionTestUtils.setField(tokenHelper,"appName","Bookmarker")
+        ReflectionTestUtils.setField(tokenHelper,"secret","secret")
+        ReflectionTestUtils.setField(tokenHelper,"expiredIn",604800)
+        ReflectionTestUtils.setField(tokenHelper,"authHeader","Authorization")
+        ReflectionTestUtils.setField(tokenHelper,"timeProvider", TimeProvider())
+    }
+
+    @Test
+    fun `should generate auth token`() {
+        val token = tokenHelper.generateToken("siva@gmail.com")
+        assertThat(token).isNotNull()
+        val username = tokenHelper.getUsernameFromToken(token)
+        assertThat(username).isEqualTo("siva@gmail.com")
+    }
+}
