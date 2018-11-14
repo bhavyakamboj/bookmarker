3383dd955ae83c0f34a27d279151776fad8ac024
Siva Katamreddy <siva.katamreddy@n26.com>
2018-11-14 10:20:13 +0100

    Sonarqube jacoco fixing paths using jacoco.destFile

--- src/test/kotlin/com/sivalabs/bookmarker/security/TokenHelperTest.kt
+++ src/test/kotlin/com/sivalabs/bookmarker/security/TokenHelperTest.kt
@@ -12,11 +12,11 @@ class TokenHelperTest {
     @Before
     fun setUp() {
         tokenHelper = TokenHelper()
-        ReflectionTestUtils.setField(tokenHelper,"appName","Bookmarker")
-        ReflectionTestUtils.setField(tokenHelper,"secret","secret")
-        ReflectionTestUtils.setField(tokenHelper,"expiredIn",604800)
-        ReflectionTestUtils.setField(tokenHelper,"authHeader","Authorization")
-        ReflectionTestUtils.setField(tokenHelper,"timeProvider", TimeProvider())
+        ReflectionTestUtils.setField(tokenHelper, "appName", "Bookmarker")
+        ReflectionTestUtils.setField(tokenHelper, "secret", "secret")
+        ReflectionTestUtils.setField(tokenHelper, "expiredIn", 604800)
+        ReflectionTestUtils.setField(tokenHelper, "authHeader", "Authorization")
+        ReflectionTestUtils.setField(tokenHelper, "timeProvider", TimeProvider())
     }
 
     @Test
