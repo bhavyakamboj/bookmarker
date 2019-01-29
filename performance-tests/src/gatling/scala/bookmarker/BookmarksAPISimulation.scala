bf618a92f45b0f66cfa483ce20c34ab21733f419
Siva Katamreddy <siva.katamreddy@n26.com>
2019-01-29 17:14:26 +0100

    Gatling tests

--- performance-tests/src/gatling/scala/bookmarker/BookmarksAPISimulation.scala
+++ performance-tests/src/gatling/scala/bookmarker/BookmarksAPISimulation.scala
@@ -8,53 +8,81 @@ import scala.util.Random
 class BookmarksAPISimulation extends Simulation {
 
   val httpConf = http
-    .baseURL("http://localhost:18080")
+    .baseURL("http://localhost:8080")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-
+  val bookmarkUrlFeeder = csv("bookmarkUrls.csv").random
+  val bookmarkIdsFeeder = csv("bookmarkIds.csv").random
   val userIdFeeder = csv("userIds.csv").random
-  val newUsersFeeder = csv("newUsers.csv").random
-
 
   var randomString = Iterator.continually(Map("randstring" -> ( Random.alphanumeric.take(20).mkString )))
-  var randomInt = Iterator.continually(Map("randinteger" -> ( Random.nextInt(1000))))
+  var randomInt = Iterator.continually(Map("randinteger" -> ( Random.nextInt(5))))
 
-  val getUserById = feed(randomInt)
-               .exec(http("UserById").get("/api/users/${randinteger}"))
+  val getAllBookmarks = exec(http("AllBookmarks").get("/api/bookmarks")).pause(1)
+
+  val getBookmarksByUser = feed(userIdFeeder)
+                           .exec(http("BookmarksByUser")
+                                .get("/api/bookmarks?userId=${userId}")
+                           )
+                           .pause(1)
+
+  val getBookmarkById = feed(bookmarkIdsFeeder)
+               .exec(http("BookmarkById").get("/api/bookmarks/${id}"))
                .pause(1)
 
-  val createUser = feed(randomString)
-                    .exec(http("Create User")
-                        .post("/api/users")
+  var accessToken = ""
+
+  val login = exec(http("Create Bookmark")
+                .post("/api/auth/login")
+                .body(StringBody(
+                  """
+                    {
+                      "username":"siva@gmail.com",
+                      "password":"siva"
+                    }
+                  """)).asJSON
+                .check(status.is(200),jsonPath("$..access_token").saveAs("accessToken"))
+                )
+                .exec(session => {
+                    accessToken = session("accessToken").as[String].trim
+                    session
+                  }
+                )
+                .pause(1)
+
+  val createBookmark = feed(bookmarkUrlFeeder)
+                    .exec(_.set("accessToken", accessToken))
+                    .exec(http("Create Bookmark")
+                        .post("/api/bookmarks")
+                        .header("Authorization", "Bearer ${accessToken}")
                         .body(StringBody(
                           """
-                            { "name":"${randstring}",
-                            "email":"${randstring}@gmail.com",
-                            "password":"${randstring}"
+                            {
+                              "url":"${url}"
                             }
                           """)).asJSON
                     )
                     .pause(1)
 
-  val deleteUserById = feed(randomInt)
-    .exec(http("DeleteUser").delete("/api/users/${randinteger}"))
-    .pause(1)
-
   // Now, we can write the scenario as a composition
-  val scnGetUserById = scenario("Get User Profile By Id").exec(getUserById).pause(2)
-  val scnCreateUser = scenario("Create New User").exec(createUser).pause(2)
-  val scnDeleteUser = scenario("Delete User By Id").exec(deleteUserById).pause(2)
+  val scnGetAllBookmarks = scenario("Get All Bookmarks").exec(getAllBookmarks).pause(2)
+  val scnGetBookmarksByUser = scenario("Get Bookmarks By User").exec(getBookmarksByUser).pause(2)
+  val scnGetBookmarkById = scenario("Get Bookmark By Id").exec(getBookmarkById).pause(2)
+  val scnLogin = scenario("Login").exec(login).pause(2)
+  val scnCreateBookmark = scenario("Create New Bookmark").exec(createBookmark).pause(2)
 
   //setUp(scn.inject(atOnceUsers(10)).protocols(httpConf))
 
   setUp(
-      scnGetUserById.inject(rampUsers(50) over (10 seconds)),
-      scnCreateUser.inject(rampUsers(50) over (10 seconds)),
-      scnDeleteUser.inject(rampUsers(5) over (10 seconds))
+      scnGetAllBookmarks.inject(rampUsers(50) over (10 seconds)),
+      scnGetBookmarksByUser.inject(rampUsers(50) over (10 seconds)),
+      scnGetBookmarkById.inject(rampUsers(50) over (10 seconds)),
+      scnLogin.inject(rampUsers(2) over (10 seconds)),
+      scnCreateBookmark.inject(rampUsers(20) over (10 seconds))
   ).protocols(httpConf)
 
 }
--- spring-boot-bookmarker-api/src/main/kotlin/com/sivalabs/bookmarker/service/BookmarkService.kt
+++ spring-boot-bookmarker-api/src/main/kotlin/com/sivalabs/bookmarker/service/BookmarkService.kt
@@ -3,6 +3,7 @@ package com.sivalabs.bookmarker.service
 import com.sivalabs.bookmarker.entity.Bookmark
 import com.sivalabs.bookmarker.model.BookmarkDTO
 import com.sivalabs.bookmarker.repo.BookmarkRepository
+import com.sivalabs.bookmarker.utils.logger
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,24 +12,29 @@ import org.jsoup.Jsoup
 @Service
 @Transactional
 class BookmarkService(private val bookmarkRepository: BookmarkRepository) {
+    private val log = logger()
 
     fun getAllBookmarks(): List<BookmarkDTO> {
+        log.debug("process=get_all_bookmarks")
         val sort = Sort.by(Sort.Direction.DESC, "createdAt")
         return bookmarkRepository.findAll(sort).map { BookmarkDTO.fromEntity(it) }
     }
 
     fun getBookmarksByUser(userId: Long): List<BookmarkDTO> {
+        log.debug("process=get_bookmarks_by_user_id, user_id=$userId")
         val sort = Sort.by(Sort.Direction.DESC, "createdAt")
         return bookmarkRepository.findByCreatedById(userId, sort).map { BookmarkDTO.fromEntity(it) }
     }
 
     fun getBookmarkById(id: Long): BookmarkDTO? {
+        log.debug("process=get_bookmark_by_id, id=$id")
         return bookmarkRepository.findById(id)
                 .map { BookmarkDTO.fromEntity(it) }
                 .orElse(null)
     }
 
     fun createBookmark(bookmark: Bookmark) {
+        log.debug("process=create_bookmark, url=${bookmark.url}")
         if (bookmark.title.isEmpty()) {
             val doc = Jsoup.connect(bookmark.url).get()
             bookmark.title = doc.title()
@@ -37,6 +43,7 @@ class BookmarkService(private val bookmarkRepository: BookmarkRepository) {
     }
 
     fun deleteBookmark(id: Long) {
+        log.debug("process=delete_bookmark_by_id, id=$id")
         bookmarkRepository.deleteById(id)
     }
 }
